7aae5765e36becd375e3f71484937cf7bd9cc40a
Juri <juri.strumpflohner@gmail.com>
2014-04-15 17:41:08 +0200

    about to implement basic layout of canjs

--- canjs/issues.js
+++ canjs/issues.js
@@ -1,9 +1,9 @@
 requirejs(['can/util/string', 'can/control'], function(can){
-  var App = can.Control({
-    init : function(){
-      this.element.html('Hello World!');
-    }
-  })
+    var App = can.Control({
+        init : function(){
+          this.element.html('Hello World!');
+        }
+    });
 
-  new App('#content');
+    new App('#js-content-container');
 });
