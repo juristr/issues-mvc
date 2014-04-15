df70fe23cda4ba1fd2fcbfa507ac656e4a38198a
Juri <juri.strumpflohner@gmail.com>
2014-04-15 13:51:17 +0200

    adds initial canjs app created by the yeoman generator

---
+++ canjs/requirejsconfig.js
@@ -0,0 +1,8 @@
+requirejs.config({
+  paths: {
+    can      : 'bower_components/canjs/amd/can',
+    jquery   : 'bower_components/jquery/jquery',
+    mustache : 'bower_components/require-can-renderers/lib/mustache',
+    ejs      : 'bower_components/require-can-renderers/lib/ejs'
+  }
+});
