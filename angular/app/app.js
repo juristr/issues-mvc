91b769cb1f20c3684dca34befd214b5878826520
Juri <juri.strumpflohner@gmail.com>
2014-05-14 16:21:28 +0200

    adds angular includes and creates issues app

---
+++ angular/app/app.js
@@ -0,0 +1,25 @@
+(function(){
+
+  'use strict';
+
+  angular.module('issuesApp', []);
+
+// angular
+//   .module('issuesApp', [
+//     'ngCookies',
+//     'ngResource',
+//     'ngSanitize',
+//     'ngRoute'
+//   ])
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'views/main.html',
+//         controller: 'MainCtrl'
+//       })
+//       .otherwise({
+//         redirectTo: '/'
+//       });
+//   });
+
+})();
