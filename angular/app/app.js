8bb62cf295129b64d79e6c8836cb966d0a947bcb
Juri <juri.strumpflohner@gmail.com>
2014-05-15 16:45:04 +0200

    fixes issue detail logic like opening/closing, adding/removing comments

--- angular/app/app.js
+++ angular/app/app.js
@@ -37,6 +37,19 @@
       ];
     }]);
 
+
+  var guid = (function() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+                 .toString(16)
+                 .substring(1);
+    }
+    return function() {
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+             s4() + '-' + s4() + s4() + s4();
+    };
+  })();
+
   angular.module('issuesApp')
     .controller('RequestsDetailsController', ['$scope', function($scope){
       $scope.item = {
@@ -62,22 +75,56 @@
         };
 
       $scope.close = function(item){
-        console.log('close item');
+
+        // write a log
+        $scope.item.comments.push({
+          comment: 'closed',
+          author: 'Juri',
+          systemLog: true,
+          lastUpdated: new Date()
+        });
+
+        // update current object
+        $scope.item.status = 'closed';
       };
+
       $scope.reOpen = function(item){
-        console.log('reopen item');
+
+        // write a log
+        $scope.item.comments.push({
+          comment: 'open',
+          author: 'Juri',
+          systemLog: true,
+          lastUpdated: new Date()
+        });
+
+        $scope.item.status = 'open';
       };
 
       $scope.addComment = function(commentBody){
         $scope.item.comments.push({
+          id: guid(),
           comment: commentBody,
           author: 'Juri',
           systemLog: false,
           lastUpdated: new Date()
         });
       };
-      $scope.removeComment = function(){
 
+      $scope.removeComment = function(comment){
+        var result = $scope.item.comments.filter(function(element){
+          return element.id === comment.id;
+        });
+
+        // delete record from array...don't blame me -.-
+        var newArray = [];
+        for(var i=0; i<$scope.item.comments.length; i++){
+          if($scope.item.comments[i].id !== comment.id){
+            newArray.push($scope.item.comments[i]);
+          }
+        }
+
+        $scope.item.comments = newArray;
       };
 
     }]);
